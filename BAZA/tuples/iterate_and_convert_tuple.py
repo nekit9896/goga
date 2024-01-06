667cedf40705cf263f1828df2747dc28d120a60c
n.vladimirov <n.vladimirov@corp.mail.ru>
2024-01-06 15:57:51 +0300

    tuples

---
+++ BAZA/tuples/iterate_and_convert_tuple.py
@@ -0,0 +1,13 @@
+data = (1, 3, "gg", 4, 54, 55.7)
+
+for i in data:
+    print(i)
+
+# Конвертировать список в кортеж
+nums = ["gg", 4, 54]
+tuple_nums = tuple(nums)
+print(tuple_nums)
+
+# Конвертировать строку в кортеж
+word = tuple("Hullo")
+print(word)
