98058759cffcf5ac4658d48a871626c3f8ce9c37
n.vladimirov <n.vladimirov@corp.mail.ru>
2024-01-27 14:03:36 +0300

    zip

---
+++ BAZA/a12_lists_functions_and_methods/enumerate.py
@@ -0,0 +1,6 @@
+for number, student in enumerate(
+    ["Nikita", "Tanya", "Masha", "Olya", "Tolya", "Misha"]
+):
+    print(number, student)
+
+print(*enumerate(["Nikita", "Tanya", "Masha", "Olya", "Tolya", "Misha"]))
